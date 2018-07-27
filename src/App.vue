3eda46e0498c1904f2c43166d3219feebd482d3d
Nora Brown <nora@Noras-MBP.hsd1.ma.comcast.net>
2018-07-27 13:29:22 -0400

    add tags styles; whitespace in css

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 
     <FigureStandardRF
       src="red-tailed-hawk.jpg"
-      type="modern"
+      type="post-it"
       :tags="['bird','hawk','raptor']"
       @click="alertHello" >Red-tailed hawk.</FigureStandardRF>
 
@@ -54,78 +54,80 @@ export default {
 </script>
 
 <style>
-*{
-  box-sizing: border-box;
-}
-#app {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  font-size: 1.2em;
-  grid-gap: 2em;
-}
-figure {
-  position: relative;
-  margin: 0;
-  padding: 0;
-}
-img {
-  max-width: 100%;
-  display: block;
-}
-figcaption {
-  position: absolute;
-}
-.tags {
-  position: absolute;
-  top: .2em;
-  right: .2em;
-  font-size: smaller;
-  color: #FFF;
-}
-.tags span{
-  background: rgbs(255,255,255,.3);
-  padding: .1em .3em;
-  margin-left: .3em;
-}
-.modern {
-  font-family: HelveticaNeue, sans-serif;
-  box-shadow: 0 8px 20px rgba(0,0,0, 0.3);
-}
-.modern figcaption{
-  bottom: 0;
-  width: 100%;
-  padding: .5em;
-  color: #FFF;
-  background: rgba(0,0,0,.8);
-}
-.post-it{
-  box-shadow: 1px 1px 3px rgba(0,0,0,.5);
-  transform: rotate(2deg);
-}
-.post-it figcaption{
-  width: 10em;
-  height: 6em;
-  bottom: 2em;
-  right: 2em;
-  padding: .5em;
-  font-family: "Bradley Hand", "Comic Sans", fantasy;
-  text-align: center;
-  background: yellow;
-  box-shadow: 1px 1px 3px rgba(0,0,0,.5);
-  transform: rotate(-7deg);
-}
-.eighties figcaption{
-  top: 60%;
-  left: -10px;
-  font-size: 2em;
-  font-family: impact;
-  transform: skew(-13deg);
-  text-indent: .5em;
-}
-.eighties figcaption span{
-  background: #4276c7;
-  color: #FFF;
-  padding: 0 .5em;
-}
+  body {
+    font-family: HelveticaNeue, sans-serif;
+  }
 
+  * {
+    box-sizing: border-box;
+  }
+  #app {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    font-size: 1.2em;
+    grid-gap: 2em;
+  }
+  figure {
+    position: relative;
+    margin: 0;
+    padding: 0;
+  }
+  img {
+    max-width: 100%;
+    display: block;
+  }
+  figcaption {
+    position: absolute;
+  }
+  .tags {
+    position: absolute;
+    top: .2em;
+    right: .2em;
+    font-size: smaller;
+    color: #FFF;
+  }
+  .tags span {
+    background: rgba(255,255,255,0.3);
+    padding: .1em .3em;
+    margin-left: .3em;
+  }
+  .modern {
+    box-shadow: 0 8px 20px rgba(0,0,0,0.3);
+  }
+  .modern figcaption {
+    bottom: 0;
+    width: 100%;
+    padding: .5em;
+    color: #FFF;
+    background: rgba(0,0,0,.8);
+  }
+  .post-it {
+    box-shadow: 1px 1px 3px rgba(0,0,0,0.5);
+    transform: rotate(2deg);
+  }
+  .post-it figcaption {
+    width: 10em;
+    height: 6em;
+    bottom: 2em;
+    right: 2em;
+    padding: .5em;
+    font-family: "Bradley Hand", "Comic Sans", fantasy;
+    text-align: center;
+    background: yellow;
+    box-shadow: 1px 1px 3px rgba(0,0,0,.5);
+    transform: rotate(-7deg);
+  }
+  .eighties figcaption {
+    top: 60%;
+    left: -10px;
+    font-size: 2em;
+    font-family: impact;
+    transform: skew(-13deg);
+    text-indent: .5em;
+  }
+  .eighties figcaption span {
+    background: #4276c7;
+    color: #FFF;
+    padding: 0 .5em;
+  }
 </style>
